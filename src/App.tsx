7156517fa14310fceddca5588c7eabf325ad18d3
RAFAEL BERNHEIM <rafaelbernheimpro@gmail.com>
2025-04-24 15:09:51 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Description from "./pages/Description";
+
+function App() {
+	// const name = "toto";
+	// const text = "Welcome";
+	return (
+		<>
+			{/* {text} {name} */}
+			<Description />
+		</>
+	);
+}
+
+export default App;
