e0e3d6fa442b940878db1f22a5ae4bc9b8b234ef
jimmy <jimmy.balay97@gmail.com>
2025-04-28 10:01:57 +0200

    fix

--- src/pages/Watchlist.tsx
+++ src/pages/Watchlist.tsx
@@ -10,6 +10,19 @@ function Watchlist() {
         <button type="button">Vus</button>
       </div>
 
+      <section className="seiya">
+        <div className="image1">
+          <img src={saintseiya} alt="" />
+          <div>
+            <p>SAINT SEIYA/LES CHEVALIERS DU ZODIAQUE</p>
+            <p>Doublage-Sous titres</p>
+          </div>
+          <div className="bouton2">
+            <button type="button">Vu</button>
+            <button type="button">Supprimer</button>
+          </div>
+        </div>
+      </section>
       <section>
         <div className="image1">
           <img src={saintseiya} alt="" />
@@ -24,7 +37,7 @@ function Watchlist() {
         </div>
       </section>
 
-      <section>
+      <section className="goku">
         <div className="image1">
           <img src={dbz} alt="" />
           <div>
