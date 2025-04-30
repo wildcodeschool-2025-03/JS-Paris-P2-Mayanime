7a829717737bf7a14e2116fc59a22ed437bd8434
jimmy <jimmy.balay97@gmail.com>
2025-04-30 10:10:18 +0200

    fix-conflict

--- src/pages/Watchlist.tsx
+++ src/pages/Watchlist.tsx
@@ -1,44 +1,36 @@
 import dbz from "../assets/DBZ.jpg";
 import saintseiya from "../assets/Saint Seiya.png";
-import "./Watchlist.css";
-
+import { useEffect, useState } from "react";
 function Watchlist() {
-  return (
-    <>
-      <div className="bouton">
-        <button type="button">Watchlist</button>
-        <button type="button">Vus</button>
-      </div>
-
-      <section>
-        <div className="image1">
-          <img src={saintseiya} alt="" />
-          <div>
-            <p>SAINT SEIYA/LES CHEVALIERS DU ZODIAQUE</p>
-            <p>Doublage-Sous titres</p>
-          </div>
-          <div>
-            <button type="button">Vu</button>
-            <button type="button">Supprimer</button>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div className="image1">
-          <img src={dbz} alt="" />
-          <div>
-            <p>DRAGON BALL Z</p>
-            <p>Doublage-Sous titres</p>
-          </div>
-          <div className="bouton2">
-            <button type="button">Vu</button>
-            <button type="button">Supprimer</button>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+	const [animeList, setAnimeList] = useState([])
+	useEffect (()=> {
+		fetch("https://api.jikan.moe/v4/anime").then((response) => response.json()).then((fullData) => setAnimeList(fullData.data))
+	}, [])
+	console.log (animeList)
+	return (
+		<>
+		{animeList.map ((anime)=>(
+		<section className="seiya">
+				<div className="image1">
+					<img src={anime.images.webp.image_url} alt="" />
+					<div>
+						<p>{anime.title_japanese}</p>
+						<p>{anime.title_english}</p>
+						<p>Doublage-Sous titres</p>
+					</div>
+					<div className="bouton2">
+						<button type="button">Vu</button>
+						<button type="button">Supprimer</button>
+					</div>
+				</div>
+			</section>	
+		))}
+			<div className="bouton">
+				<button type="button">Watchlist</button>
+				<button type="button">Vus</button>
+			</div>
+		</>
+	);
 }
 
 export default Watchlist;
