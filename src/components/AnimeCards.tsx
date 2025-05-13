e92ac173073642d186bb1d1eb11bc6ca4275e159
Hamidou <hake92240@gmail.com>
2025-05-13 16:34:02 +0200

    navigate

--- src/components/AnimeCards.tsx
+++ src/components/AnimeCards.tsx
@@ -1,58 +1,64 @@
 import { useEffect, useState } from "react";
 import "../pages/Catalogue.css";
+import { useNavigate } from "react-router-dom";
 
 interface Anime {
-  mal_id: number;
-  title: string;
-  score: number;
-  episodes: number;
-  images: {
-    jpg: {
-      image_url: string;
-    };
-  };
+	mal_id: number;
+	title: string;
+	score: number;
+	episodes: number;
+	images: {
+		jpg: {
+			image_url: string;
+		};
+	};
 }
 
 function AnimeCards({ checked, search }) {
-  // console.log(checked, name);
+	// console.log(checked, name);
+	const navigate = useNavigate();
 
-  const [data, setData] = useState<Anime[]>([]);
+	const [data, setData] = useState<Anime[]>([]);
 
-  useEffect(() => {
-    fetch("https://api.jikan.moe/v4/anime")
-      .then((response) => response.json())
-      .then((data) => setData(data.data));
-  }, []);
+	useEffect(() => {
+		fetch("https://api.jikan.moe/v4/anime")
+			.then((response) => response.json())
+			.then((data) => setData(data.data));
+	}, []);
 
-  const dataFiltered = data.filter((anime) => {
-    if (checked) {
-      return anime.status === "Currently Airing";
-    }
-    return true;
-  });
+	const dataFiltered = data.filter((anime) => {
+		if (checked) {
+			return anime.status === "Currently Airing";
+		}
+		return true;
+	});
 
-  const result = dataFiltered.filter((el) =>
-    el.title.toLowerCase().includes(search.toLowerCase())
-  );
-  //  console.info(result);
-  return (
-    <>
-      {result.map((anime) => (
-        <article key={anime.mal_id}>
-          <a href="/">
-            <img src={anime.images.jpg.image_url} alt="" />
-          </a>
-          <p>{anime.title}</p>
-          <p>{anime.score}⭐</p>
-          <p>{anime.episodes}🎞️</p>
-          <div className="redirection">
-            <button type="button">En savoir plus</button>
-            <button type="button">Ajouter à la Watchlist ​🕛​​</button>
-          </div>
-        </article>
-      ))}
-    </>
-  );
+	const result = dataFiltered.filter((el) =>
+		el.title.toLowerCase().includes(search.toLowerCase()),
+	);
+	//  console.info(result);
+	return (
+		<>
+			{result.map((anime) => (
+				<article key={anime.mal_id}>
+					<a href="/">
+						<img src={anime.images.jpg.image_url} alt="" />
+					</a>
+					<p>{anime.title}</p>
+					<p>{anime.score}⭐</p>
+					<p>{anime.episodes}🎞️</p>
+					<div className="redirection">
+						<button onClick={() => navigate("/Description")} type="button">
+							En savoir plus
+						</button>
+						<button onClick={() => navigate("/Watchlist")} type="button">
+							Ajouter à la Watchlist ​🕛​​
+						</button>
+					</div>
+				</article>
+			))}
+		</>
+	);
 }
 
 export default AnimeCards;
