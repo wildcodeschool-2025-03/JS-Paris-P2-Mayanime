fcbf42fbf6d12193bbf1d010a01aa805407175f9
Omaya404 <omaya.battour@gmail.com>
2025-05-14 21:24:53 +0200

    add condition in watchlist's page

--- src/components/AddWatchlistButton.tsx
+++ src/components/AddWatchlistButton.tsx
@@ -2,15 +2,25 @@ import { useContext } from "react";
 import selectionWatchlistContext from "../context/selectionWatchlist";
 
 function AddWatchlistButton({ anime }) {
-	const { selectionWatchlist, setSelectionWatchlist } = useContext(
-		selectionWatchlistContext,
-	);
+	const {
+		selectionWatchlist,
+		setSelectionWatchlist,
+		isItSelected,
+		removeSelection,
+	} = useContext(selectionWatchlistContext);
 	return (
 		<button
 			type="button"
-			onClick={() => setSelectionWatchlist([...selectionWatchlist, anime])}
+			onClick={() =>
+				!isItSelected(anime)
+					? setSelectionWatchlist([...selectionWatchlist, anime])
+					: removeSelection(anime)
+			}
 		>
-			Ajouter à la Watchlist ​🕛​​
+			{!isItSelected(anime)
+				? "Ajouter à la Watchlist ​🕛"
+				: "Supprimer de la Watchlist "}
+			​​
 		</button>
 	);
 }
