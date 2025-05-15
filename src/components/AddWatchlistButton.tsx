072ad04a9bf2d51cb6de00494ae2f6b95d5749fe
jimmy <jimmy.balay97@gmail.com>
2025-05-15 10:13:03 +0200

    suiteContext

--- src/components/AddWatchlistButton.tsx
+++ src/components/AddWatchlistButton.tsx
@@ -17,11 +17,13 @@ function AddWatchlistButton({ anime }) {
 					: removeSelection(anime)
 			}
 		>
+				
 			{!isItSelected(anime)
 				? "Ajouter à la Watchlist ​🕛"
 				: "Supprimer de la Watchlist "}
 			​​
 		</button>
+				
 	);
 }
 export default AddWatchlistButton;
